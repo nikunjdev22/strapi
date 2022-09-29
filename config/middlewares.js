b76eb4f2db25d157f846ef9c1e82f20fc890dcdb
IPS-150 <nikunjc.itpathsolutions@gmail.com>
2022-09-29 15:24:05 +0530

    intial commit

---
+++ config/middlewares.js
@@ -0,0 +1,22 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
+// module.exports = {
+//   //...
+//   settings: {
+//     cors: {
+//       origin: ['*'], //allow all origins
+//       headers: ['*'], //allow all headers
+//     },
+//   },
+// };
